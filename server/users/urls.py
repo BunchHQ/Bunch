d240313679a0488904ec11c6c51c6d40ffb4f757
Harsh Narayan Jha <harshnj@proton.me>
2025-03-26 14:26:38 +0530

    server: remove the default token auth and add rest_framework_simplejwt for JWT based auth

--- server/users/urls.py
+++ server/users/urls.py
@@ -1,7 +1,10 @@
 from rest_framework import routers
 
-from users.views import GroupViewSet, UserViewSet
+from users.views import UserViewSet
+
+app_name = "users"
 
 router = routers.DefaultRouter()
-router.register(r"user", UserViewSet, basename='user')
-router.register(r"group", GroupViewSet, basename='group')
+router.register(r"user", UserViewSet, basename="user")
+# unload groups for now
+# router.register(r"group", GroupViewSet, basename='group')
