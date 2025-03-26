ea5b642cca5dd279bf74c80686a0160fc45c779d
Harsh Narayan Jha <harshnj@proton.me>
2025-03-26 14:26:38 +0530

    server: remove the default token auth and add rest_framework_simplejwt for JWT based auth

--- server/bunch/settings.py
+++ server/bunch/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "rest_framework.authtoken",
+    "rest_framework_simplejwt",
     "core",
     "users",
 ]
@@ -136,6 +136,16 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ],
 }
+
+if DEBUG:
+    from datetime import timedelta
+
+    SIMPLE_JWT = {
+        "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),
+        "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+        
+    }
--- server/users/models.py
+++ server/users/models.py
@@ -59,7 +59,7 @@ class User(AbstractUser):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True)
-    avatar = models.ImageField(upload_to="avatars/", blank=True, null=False, help_text="User's profile picture")
+    avatar = models.ImageField(upload_to="avatars/", blank=True, null=True, help_text="User's profile picture")
     status = models.CharField(blank=True, null=True, max_length=30, help_text="User's status")
     bio = models.TextField(blank=True, null=True, help_text="User's bio")
 
@@ -68,8 +68,11 @@ class User(AbstractUser):
         help_text="User's theme preference",
         choices=ThemePreferenceChoices.choices,
         default=ThemePreferenceChoices.SYSTEM,
+        blank=True,
     )
-    color = models.CharField(max_length=7, help_text="User's color preference", choices=ColorChoices.choices)
+    color = models.CharField(
+        max_length=7, help_text="User's color preference", choices=ColorChoices.choices, blank=True
+    )
     pronoun = models.CharField(blank=True, null=True, max_length=12, help_text="User's pronoun")
 
     @override
