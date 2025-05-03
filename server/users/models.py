34067c6e60b02cecc611f86e83b7466dc16a1d40
Harsh Narayan Jha <harshnj@proton.me>
2025-05-03 12:37:03 +0530

    server: change default ordering on models to be `-date_joined` or `-updated_at` or similar
    fix: channels ordering was reversed

--- server/users/models.py
+++ server/users/models.py
@@ -127,6 +127,7 @@ class User(AbstractUser):
     class Meta:
         verbose_name = "User"
         verbose_name_plural = "Users"
+        ordering = ["-date_joined"]
 
     if TYPE_CHECKING:
         owned_bunches: models.QuerySet["Bunch"]
