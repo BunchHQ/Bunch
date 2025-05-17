c5cf3d17938a8c5cf41f80ba412afe000c8626b1
Harsh Narayan Jha <harshnj@proton.me>
2025-05-17 20:28:56 +0530

    server: add post_save signal to bunch to create owner member

--- server/bunch/apps.py
+++ server/bunch/apps.py
@@ -1,6 +1,14 @@
+from typing import override
+
 from django.apps import AppConfig
 
 
 class BunchConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "bunch"
+
+    @override
+    def ready(self) -> None:
+        import bunch.signals  # noqa
+
+        return super().ready()
