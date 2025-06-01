69ec4704ce7afdc735ad6a4e7934f652dee4fab5
Harsh Narayan Jha <harshnj@proton.me>
2025-06-01 16:49:45 +0530

    docs: add brand theming and icons

--- docs/src/App.svelte
+++ docs/src/App.svelte
@@ -1,6 +1,71 @@
 <script lang="ts">
 </script>
 
+<svelte:head>
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+  <link
+    rel="apple-touch-icon"
+    sizes="180x180"
+    href="/apple-touch-icon-light.png"
+    media="(prefers-color-scheme: light)"
+  />
+  <link
+    rel="apple-touch-icon"
+    sizes="180x180"
+    href="/apple-touch-icon-dark.png"
+    media="(prefers-color-scheme: dark)"
+  />
+
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="32x32"
+    href="/favicon-32x32-dark.png"
+    media="(prefers-color-scheme: dark)"
+  />
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="32x32"
+    href="/favicon-32x32-light.png"
+    media="(prefers-color-scheme: light)"
+  />
+
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="16x16"
+    href="/favicon-16x16-dark.png"
+    media="(prefers-color-scheme: dark)"
+  />
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="16x16"
+    href="/favicon-16x16-light.png"
+    media="(prefers-color-scheme: light)"
+  />
+
+  <!-- Default favicon (for browsers that don't support media queries in link tags (aka firefox)) -->
+  <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+  <link
+    rel="icon"
+    type="image/x-icon"
+    href="/favicon-light.ico"
+    media="(prefers-color-scheme: light)"
+  />
+  <link
+    rel="icon"
+    type="image/x-icon"
+    href="/favicon-dark.ico"
+    media="(prefers-color-scheme: dark)"
+  />
+
+  <link rel="manifest" href="/site.webmanifest" />
+
+  <title>Bunch!</title>
+</svelte:head>
+
 <main class="grid min-h-svh w-full place-content-center bg-blue-200">
   <h1 class="text-5xl text-blue-500">Bunch</h1>
 </main>
