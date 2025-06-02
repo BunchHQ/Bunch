870bddf71eca3fb0d66b59f34f54a36193938e3e
Harsh Narayan Jha <harshnj@proton.me>
2025-06-02 22:40:16 +0530

    docs: complete homepage
    docs: fix symlinks 1/2
    
    docs: fix symlinks 2/2
    
    docs: minor design fixes
    
    docs: add hover and active states to button component
    
    docs: switch to webp image format
    
    docs: some spicy animations

---
+++ docs/src/components/FooterUpper.svelte
@@ -0,0 +1,54 @@
+<script>
+import Button from "./ui/button.svelte"
+</script>
+
+<div class="relative grid max-w-7xl grid-cols-[1fr_0.8fr_1fr] grid-rows-1 px-4">
+  <img
+    src="/bunch_footer_line.webp"
+    alt="Footer line graphic"
+    class="absolute left-1/3 w-56"
+  />
+
+  <div class="flex flex-col items-start justify-end gap-2 px-8">
+    <h2 class="text-4xl font-bold">Discover!</h2>
+    <p class="font-bunch text-lg">
+      Whether you're into gaming, music, study groups, or just chill
+      convos—there’s a Bunch for you.
+    </p>
+    <span></span>
+    <Button class="w-full text-2xl">Browse Public Bunches</Button>
+    <img
+      src="/bunch_mascot_discover.webp"
+      alt="Bunch Mascot thumb's up"
+      class="ms-8 h-auto w-32"
+      width="128"
+      height="148"
+    />
+  </div>
+
+  <img
+    src="/bunch_mascot_start_jump.webp"
+    alt="Bunch Mascot thumb's up"
+    class="h-auto w-40 self-start justify-self-end"
+    width="160"
+    height="186"
+  />
+
+  <div class="flex flex-col items-center justify-between gap-16 px-8 pb-16">
+    <div class="space-y-4">
+      <h2 class="text-center text-4xl font-bold">Start Your Very Own Bunch</h2>
+      <p class="font-bunch text-center text-lg">
+        Create a space that’s truly yours. Set the vibe, invite your crew.
+        You’re in control of how you connect.
+      </p>
+    </div>
+    <Button class="w-full text-2xl">Create New Bunch</Button>
+    <img
+      src="/bunch_footer_dot_grid.webp"
+      alt="Footer decorative"
+      width="900"
+      height="225"
+      class="h-auto w-full"
+    />
+  </div>
+</div>
---
+++ docs/src/components/ui/button.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+import type { Snippet } from "svelte"
+import type { ClassValue } from "svelte/elements"
+
+interface ButtonProps {
+  border?: "primary" | "dotted" | "none"
+  bg?: "creme" | "primary" | "none"
+  color?: "primary" | "white" | "creme" | "light"
+  children: Snippet
+  class?: ClassValue
+}
+let props: ButtonProps = $props()
+let {
+  border = "primary",
+  bg = "primary",
+  color = "creme",
+  children,
+}: ButtonProps = props
+</script>
+
+<span
+  class={[
+    "text-nowrap rounded-xl",
+    {
+      "border-bunch-primary-dark border-4": border === "primary",
+    },
+    props.class?.includes("w-full") ? "w-full" : "w-min",
+  ]}
+>
+  <button
+    class={[
+      "font-bunch h-full cursor-pointer rounded-[calc(theme(borderRadius.xl)-4px)] px-6 py-1 pb-2 font-medium tracking-tight transition-colors duration-75",
+      {
+        "bg-bunch-accent-cream hover:bg-bunch-accent-blue hover:text-bunch-accent-cream active:bg-bunch-primary-darker":
+          bg === "creme",
+        "bg-bunch-primary-dark hover:bg-bunch-primary-darker active:bg-bunch-secondary":
+          bg === "primary",
+        "hover:bg-bunch-primary-dark active:bg-bunch-primary-dark ring-bunch-primary-darker mx-1 my-1 rounded-lg ring-0 active:ring-2":
+          bg === "none",
+        "text-bunch-accent-blue": color === "primary",
+        "text-white": color === "white",
+        "text-bunch-accent-cream": color === "creme",
+        "text-bunch-text-light dark:text-bunch-text-dark": color === "light",
+        "border-bunch-primary-darker border": border === "primary",
+        "outline-bunch-text-light dark:outline-bunch-text-dark outline-5 outline-dashed -outline-offset-2":
+          border === "dotted",
+      },
+      props.class,
+    ]}
+  >
+    {@render children()}
+  </button>
+</span>
