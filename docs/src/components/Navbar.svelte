6d20244cfed2cf18d442e8a53e2c2395d9592ae0
Harsh Narayan Jha <harshnj@proton.me>
2025-06-13 15:18:40 +0530

    docs: mobile responsiveness
    docs: make animations actually work in build

--- docs/src/components/Navbar.svelte
+++ docs/src/components/Navbar.svelte
@@ -1,4 +1,5 @@
 <script>
+import { onMount } from "svelte"
 import { circOut } from "svelte/easing"
 import { fly } from "svelte/transition"
 import Button from "./ui/button.svelte"
@@ -9,19 +10,44 @@ const links = [
   { title: "Newsletter", url: "/news" },
   { title: "About Us!", url: "/about" },
 ]
+
+let mounted = false
+onMount(() => {
+  mounted = true
+})
 </script>
 
-<nav class="fixed top-8 z-10 w-full" transition:fly={{y: -100, delay: 200, opacity: 0, easing: circOut}}>
-  <div class="m-auto flex w-max flex-row items-stretch justify-stretch gap-4">
-    <div class="bg-bunch-primary-dark rounded-xl p-[0.25rem] shadow-2xl">
+<!-- workaround for transitions not playing in build -->
+{#if mounted}
+  <nav
+    class="absolute top-8 z-10 w-full md:fixed"
+    transition:fly={{ y: -100, opacity: 0, easing: circOut }}
+  >
+    <div
+      class="m-auto flex w-max flex-col items-center justify-stretch gap-2 md:flex-row md:items-stretch md:gap-4"
+    >
       <div
-        class="bg-bunch-primary border-bunch-primary-darker rounded-[calc(theme(borderRadius.xl)-4px)] border-2"
+        class="bg-bunch-primary-dark justify-self-end rounded-xl p-[0.25rem] shadow-2xl"
       >
-        {#each links as link}
-          <Button bg="none" border="none" color="white" class="text-lg">{link.title}</Button>
-        {/each}
+        <div
+          class="bg-bunch-primary border-bunch-primary-darker border-1 rounded-[calc(theme(borderRadius.xl)-4px)] md:border-2"
+        >
+          {#each links as link}
+            <Button
+              bg="none"
+              border="none"
+              color="white"
+              innerClass="md:text-lg">{link.title}</Button
+            >
+          {/each}
+        </div>
       </div>
+      <Button
+        bg="creme"
+        color="primary"
+        class="justify-self-center shadow-xl md:justify-self-start"
+        innerClass="md:text-2xl text-xl px-4">Open Bunch</Button
+      >
     </div>
-    <Button bg="creme" color="primary" class="text-2xl shadow-xl">Open Bunch</Button>
-  </div>
-</nav>
+  </nav>
+{/if}
