67e7a11a69da45ca121428bae26d663ff960f948
Harsh Narayan Jha <harshnj@proton.me>
2025-12-27 20:35:58 +0530

    server: add first_name and last_name back to the user serializer

--- server/users/serializers.py
+++ server/users/serializers.py
@@ -18,6 +18,8 @@ class UserSerializer(serializers.ModelSerializer):
             "id",
             "email",
             "username",
+            "first_name",
+            "last_name",
             "display_name",
             "password",
             "is_active",
